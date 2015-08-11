22f96c904dc9ec3d179fd2aeb2c64ddf8ddeec97
tntim96 <tntim96@gmail.com>
2015-08-11 12:00:55 +1000

    Add support for specifying merge language (mailchimp or handlebars)

--- src/main/java/com/microtripit/mandrillapp/lutung/view/MandrillMessage.java
+++ src/main/java/com/microtripit/mandrillapp/lutung/view/MandrillMessage.java
@@ -21,6 +21,7 @@ public class MandrillMessage {
 	private String bcc_address, tracking_domain, signing_domain, 
 			return_path_domain;
 	private Boolean merge;
+	private String merge_language;
 	private List<MergeVar> global_merge_vars;
 	private List<MergeVarBucket> merge_vars;
 	private List<String> tags;
@@ -392,6 +393,22 @@ public class MandrillMessage {
 	 */
 	public void setMerge(final Boolean merge) {
 		this.merge = merge;
+	}
+
+	/**
+	 * @param merge Whether to evaluate merge tags in the message. Will
+	 * @return The merge language used for this message one of (mailchimp, handlebars). May be null.
+	 */
+	public String getMergeLanguage() {
+		return merge_language;
+	}
+
+	/**
+	 * The merge tag language to use when evaluating merge tags, either mailchimp or handlebars
+	 * @param mergeLanguage one of (mailchimp, handlebars)
+	 */
+	public void setMergeLanguage(String mergeLanguage) {
+		this.merge_language = mergeLanguage;
 	}
 
 	/**
